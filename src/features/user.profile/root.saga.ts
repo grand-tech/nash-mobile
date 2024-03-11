3d0eefec1590aa103f2f163af691a13ca474c20f
allen <allenmuhani@gmail.com>
2024-03-11 12:24:51 +0300

    remove unused code

--- src/features/user.profile/root.saga.ts
+++
@@ -1,9 +0,0 @@
-import { spawn } from "redux-saga/effects";
-import { watchSetLanguage } from "./sagas/test.saga";
-
-/**
- * Root saga of the module/feature.
- */
-export function* userProfileSagas() {
-    yield spawn(watchSetLanguage)
-}
