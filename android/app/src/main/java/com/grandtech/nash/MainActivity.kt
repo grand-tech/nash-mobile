5eb278d3240b17c711d9fc9b973eff61101f1807
allen <allenmuhani@gmail.com>
2024-03-14 22:50:56 +0300

    changed android package

--- android/app/src/main/java/com/grandtech/nash/MainActivity.kt
+++ android/app/src/main/java/com/grandtech/nash/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nash
+package com.grandtech.nash
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/grandtech/nash/MainApplication.kt
+++ android/app/src/main/java/com/grandtech/nash/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.nash
+package com.grandtech.nash
 
 import android.app.Application
 import com.facebook.react.PackageList
