d9687637677e762497f3318c46a0a9605820ee39
allen <allenmuhani@gmail.com>
2024-03-15 11:30:56 +0300

    added base animation

--- App.tsx
+++ App.tsx
@@ -7,24 +7,11 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import {Counter} from './src/features/counter/Counter';
+import RehydrationLoadingScreen from './src/components/RehydrationLoadingScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -64,18 +51,9 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <Counter></Counter>
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView style={backgroundStyle}>
+    <RehydrationLoadingScreen></RehydrationLoadingScreen>
+    // </SafeAreaView>
   );
 }
 
